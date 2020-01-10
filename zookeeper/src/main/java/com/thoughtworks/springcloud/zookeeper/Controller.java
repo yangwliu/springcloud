c91cde698748fb193eb6e2a266253b75124090db
yangwliu <yangwliu@thoughtworks.com>
2020-01-10 11:46:05 +0800

    add example for ribbon

--- zookeeper/src/main/java/com/thoughtworks/springcloud/zookeeper/Controller.java
+++ zookeeper/src/main/java/com/thoughtworks/springcloud/zookeeper/Controller.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.springcloud.zookeeper;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -7,14 +8,21 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 public class Controller {
 
-  @Autowired
-  private DiscoveryClient discoveryClient;
+  private final DiscoveryClient discoveryClient;
+  private final RestTemplate restTemplate;
+
+  public Controller(DiscoveryClient discoveryClient, RestTemplate restTemplate) {
+    this.discoveryClient = discoveryClient;
+    this.restTemplate = restTemplate;
+  }
 
   @RequestMapping("/service")
   public String getServiceUrlByName(@RequestParam("name") String name) {
@@ -23,5 +31,17 @@ public class Controller {
       return "No such service";
     }
     return instances.get(0).getUri().toString();
+  }
+
+  @RequestMapping("/hello")
+  public String sayHello() {
+
+    log.info("enter into current service!");
+    return "hello world!";
+  }
+
+  @RequestMapping("/proxy/hello")
+  public String proxyHello() {
+    return restTemplate.getForEntity("http://zookeeper-client/hello", String.class).getBody();
   }
 }
